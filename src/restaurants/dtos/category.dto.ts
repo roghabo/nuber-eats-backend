e1ac88d2e400214cdfce8199a59079f5aa7b22d2
Jinyoung <roghabo@gmail.com>
2020-12-21 12:15:28 +0900

    finish to section 11

---
+++ src/restaurants/dtos/category.dto.ts
@@ -0,0 +1,22 @@
+import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
+import { ArgsType, Field, InputType, ObjectType } from '@nestjs/graphql';
+import { CoreOutput } from 'src/common/dtos/output.dto';
+import {
+  PaginationInput,
+  PaginationOutput,
+} from 'src/common/dtos/pagination.dto';
+import { Category } from '../entities/cetegory.entity';
+
+@InputType()
+export class CategoryInput extends PaginationInput {
+  @Field(type => String)
+  slug: string;
+}
+
+@ObjectType()
+export class CategoryOutput extends PaginationOutput {
+  @Field(type => [Restaurant], { nullable: true })
+  restaurants?: Restaurant[];
+  @Field(type => Category, { nullable: true })
+  category?: Category;
+}
--- src/restaurants/restaurants.module.ts
+++ src/restaurants/restaurants.module.ts
@@ -1,12 +1,12 @@
+import { CategoryRepository } from './repositories/category.repository';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Category } from './entities/cetegory.entity';
 import { Restaurant } from './entities/restaurant.entity';
 import { RestaurantResolver } from './restaurants.resolver';
 import { RestaurantService } from './restaurants.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Restaurant, Category])],
-  providers: [RestaurantResolver, RestaurantService],
+  imports: [TypeOrmModule.forFeature([Restaurant, CategoryRepository])],
+  providers: [RestaurantResolver, CategoryRepository, RestaurantService],
 })
 export class RestaurantsModule {}
